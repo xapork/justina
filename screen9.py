39f603c909ee4cde9fcd321ab9b73c093cc4d5f2
Justina Romanenko <150509333+xapork@users.noreply.github.com>
2023-12-22 22:03:06 +0500

    Delete screen9.py

--- screen9.py
+++
@@ -1,8 +0,0 @@
-def destroy_ur_enemies(names_array, enemies_array):
-    return [name for name in names_array if name not in enemies_array]
-
-names = ["tony", "peter", "paul", "john", "ringo", "love", "moriarty", "voland"]
-enemies = ["ringo", "moriarty", "voland", "love"]
-
-perfect_names = destroy_ur_enemies(names, enemies)
-print(perfect_names)
